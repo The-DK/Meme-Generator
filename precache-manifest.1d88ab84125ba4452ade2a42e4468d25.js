5c764aa6b4c4f14730beca36147b43666e7782a0
The-DK <dhanishkakkar@yahoo.com>
2020-05-24 20:06:09 -0400

    Updates

---
+++ precache-manifest.1d88ab84125ba4452ade2a42e4468d25.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfad82ccf7ea85dda9b020aad921022f",
+    "url": "/Meme-Generator/index.html"
+  },
+  {
+    "revision": "1ac8b3d6a60647649990",
+    "url": "/Meme-Generator/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "40c1b84201f20e601321",
+    "url": "/Meme-Generator/static/css/main.bcd4e4c7.chunk.css"
+  },
+  {
+    "revision": "1ac8b3d6a60647649990",
+    "url": "/Meme-Generator/static/js/2.e00e48a3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Meme-Generator/static/js/2.e00e48a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40c1b84201f20e601321",
+    "url": "/Meme-Generator/static/js/main.bb881ca8.chunk.js"
+  },
+  {
+    "revision": "12ce8ff062cd0ceeb1c7",
+    "url": "/Meme-Generator/static/js/runtime-main.29595c8e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Meme-Generator/static/media/logo.5d5d9eef.svg"
+  }
+]);
